e55e4c2913e40b544396aa9da3fcf0c40b6286a5
momozor <skelic3@gmail.com>
2019-03-07 21:19:56 +0800

    remove pagi option  and cover more tests

--- test/test_python_pixabay.py
+++ test/test_python_pixabay.py
@@ -16,6 +16,9 @@ class TestPythonPixabay(TestCase):
 
     def test_custom_image_search(self):
         self.assertIn("hits", image.search(q="water", page=1))
+        self.assertEqual(image.search(q="apple", page=1)["hits"][0]["pageURL"],
+         "https://pixabay.com/photos/apple-red-delicious-fruit-256261/")
 
     def test_custom_video_search(self):
-        self.assertIn("hits", video.search(q="water"))
+        self.assertEqual(video.search(q="apple", page=1)["hits"][0]["pageURL"],
+         "https://pixabay.com/videos/id-1019/")
