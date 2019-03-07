733a9b12b3672eb735c5c2bba794e1fc770b9d37
momozor <skelic3@gmail.com>
2019-03-08 01:27:21 +0800

    add more classifiers and add supported python version badge

--- setup.py
+++ setup.py
@@ -23,5 +23,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta'
     ])
