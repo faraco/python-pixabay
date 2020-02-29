4606aab008fffb66258d1a4d5d2f319f50c6afbd
momozor <skelic3@gmail.com>
2020-03-01 05:18:46 +0800

    rename publish.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from os import path
 
 this_dir = path.abspath(path.dirname(__file__))
-with open(path.join(this_dir, "README.md"), encoding="utf-8") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
@@ -13,16 +13,17 @@ setup(
     url='https://github.com/momozor/python-pixabay',
     author='momozor',
     author_email='skelic3@gmail.com',
-    version='4.0',
+    version='4.1',
     license='MIT',
     py_modules=['pixabay'],
     install_requires=['requests'],
     python_requires='~=3.5',
-    keywords='api development pixabay wrapper python3',
+    keywords='api development pixabay wrapper library python3',
     classifiers=[
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
