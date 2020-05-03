72d1657b5a1a09c59248119ac9385b63f3691ebd
Momozor <skelic3@gmail.com>
2020-05-04 04:22:12 +0800

    Update test

--- test/test_python_pixabay.py
+++ test/test_python_pixabay.py
@@ -23,7 +23,7 @@ class TestPythonPixabay(TestCase):
             image.search(q="apple",
                          page=1,
                          safesearch="false",
-                         editors_choice="true")["totalHits"], 152)
+                         editors_choice="true")["totalHits"], 154)
 
     def test_custom_video_search(self):
         self.assertEqual(
